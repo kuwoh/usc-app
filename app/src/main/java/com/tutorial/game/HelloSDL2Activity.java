f59529f0698ef7d86b8e8e9f552ad22499bf8919
Ian <14359681+kuwoh@users.noreply.github.com>
2024-04-19 17:00:57 +0800

    -c

--- app/src/main/java/com/tutorial/game/HelloSDL2Activity.java
+++ app/src/main/java/com/tutorial/game/HelloSDL2Activity.java
@@ -1,7 +1,6 @@
 package com.tutorial.game;
 
 import org.libsdl.app.SDLActivity;
-import com.example.unnamed_sdvx_clone.provider.DocumentsProvider;
 
 public class HelloSDL2Activity extends SDLActivity
 {
