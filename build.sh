a20274f108226df883f5991fa4dd475232e7dc86
Ian <14359681+kuwoh@users.noreply.github.com>
2024-10-17 01:00:58 +0800

    revert

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 echo "
 set(VCPKG_TARGET_ARCHITECTURE arm64)
-set(VCPKG_CRT_LINKAGE static)
-set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_CMAKE_SYSTEM_NAME Android)
 set(VCPKG_LIBRARY_LINKAGE static)
 " > ./vcpkg/triplets/community/arm64-android.cmake
-//line-default 3-dynamic 4-dynamic
+
 ./gradlew assembleDebug
